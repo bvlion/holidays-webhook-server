f1284a0fb6180e67c3ab9a57fecdb1fa752e7025
bvlion <bvltiggeari@ambitious-i.net>
2021-09-03 12:53:09 +0900

    テスト結果を html に変換する

--- lib/google_client_export.php
+++ lib/google_client_export.php
@@ -1,6 +1,7 @@
 <?php // .env の GOOGLE_CLIENT_* を書き換える
+$current = dirname(__FILE__);
 
-$hidden_file = 'google_client_secret';
+$hidden_file = $current . '/google_client_secret';
 
 if (!file_exists($hidden_file)) {
   return;
@@ -9,7 +10,7 @@ if (!file_exists($hidden_file)) {
 $content = file_get_contents($hidden_file);
 $rows = explode("\n", $content);
 
-$env_file = 'src/.env';
+$env_file = $current . '/../src/.env';
 $str = file_get_contents($env_file);
  
 $str = preg_replace('/GOOGLE_CLIENT_ID.*/', $rows[0], $str);
