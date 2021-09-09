1b99ca8c6ce0dd085306421907073fd53a2bc5f6
bvlion <bvltiggeari@ambitious-i.net>
2021-09-09 16:52:57 +0900

    お試し API を実装

---
+++ src/app/Http/Controllers/Api/CommandExecController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use App\Models\User;
+use App\Models\Command;
+use App\Models\SummarizeCommand;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class CommandExecController extends Controller
+{
+  public function command(Request $request, int $id)
+  {
+    $command = Command::find($id);
+
+    if ($command === null || $command->deleted_at !== null) {
+      throw new HttpException(404, 'Don\'t found command');
+    }
+
+    $this->checkExecutableUser(
+      $command->target_type,
+      $command->target_id,
+      $request->user()
+    );
+
+    return $this->exec([$command]);
+  }
+
+  public function summary(Request $request, int $id)
+  {
+    $summarize_command = SummarizeCommand::find($id);
+
+    if ($summarize_command === null || $summarize_command->deleted_at !== null) {
+      throw new HttpException(404, 'Don\'t found summarize command');
+    }
+
+    $this->checkExecutableUser(
+      $summarize_command->target_type,
+      $summarize_command->target_id,
+      $request->user()
+    );
+
+    return $this->exec(
+      Command::whereIn(
+        'id', json_decode($summarize_command->commands, true)
+      )->get()->all()
+    );
+  }
+
+  private function checkExecutableUser(string $target_type, int $target_id, User $user)
+  {
+    if (
+      ($target_type == 'user' && $target_id == $user->id) ||
+      ($target_type == 'group' && $target_id == $user->groups_id)
+    ) {
+      return;
+    }
+    throw new HttpException(403, 'Haven\'t execute permission');
+  }
+
+  private function exec(array $commands)
+  {
+    $results = [];
+
+    foreach ($commands as $command) {
+      $client = new Client();
+
+      $options = [];
+      $options['allow_redirects'] = true;
+
+      $headers = json_decode($command->headers, true);
+      if (!empty($headers)) {
+        $options['headers'] = $headers;
+      }
+
+      if (!empty($command->parameters)) {
+        $parameter = str_replace('##DATETIME##', date('Y-m-d H:i:s'), $command->parameters);
+        if ($command->body_type == 'json') {
+          $options[$command->body_type] = $parameter;
+        } else {
+          $options[$command->body_type] = json_decode($parameter, true);
+        }
+      }
+
+      try {
+        $res = $client->request($command->method, $command->url, $options);
+        array_push($results, $this->saveResult($command->target_name, $res));
+      } catch (RequestException $e) {
+        array_push($results, $this->saveResult($command->target_name, $e->getResponse()));
+      }
+    }
+
+    return $results;
+  }
+
+  private function saveResult(string $name, ResponseInterface $res) {
+    return [
+      'name' => $name,
+      'response_code' => $res->getStatusCode(),
+      'response_header' => json_encode($res->getHeaders(), true),
+      'response_body' => $res->getBody(),
+    ];
+  }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
   return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function () {
+  Route::post('/exec/command/{id}', 'CommandExecController@command');
+  Route::post('/exec/summary/{id}', 'CommandExecController@summary');
+});
