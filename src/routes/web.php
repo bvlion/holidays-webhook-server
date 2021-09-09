2888a42b32203854d709f611045b4deec5302bf9
bvlion <bvltiggeari@ambitious-i.net>
2021-09-09 14:49:03 +0900

    ディレクトリ、namespace を web と api に分割

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\GoogleLoginController;
+use App\Http\Controllers\Web\GoogleLoginController;
 
 Route::get('/', function () {
   return [
