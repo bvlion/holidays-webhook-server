513af839809357318c5d83ae08f1fd18ed3df80b
bvlion <bvltiggeari@ambitious-i.net>
2021-09-13 16:27:05 +0900

    不要な API を削除

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-  return $request->user();
-});
-
 Route::group(['middleware' => 'auth:api'], function () {
   Route::post('/exec/command/{id}', 'CommandExecController@command');
   Route::post('/exec/summary/{id}', 'CommandExecController@summary');
