c8763d60abfec881ddd3084c8f5ab8036d450eab
bvlion <bvltiggeari@ambitious-i.net>
2021-10-17 15:15:35 +0900

    API エラーは JSON を返すように修正

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {   
+        if ($request->is('api/*') && $e instanceof HttpException) {
+            return response()->json(['error' => $e->getMessage()], $e->getStatusCode());
+        }
+        return parent::render($request, $e);
     }
 }
